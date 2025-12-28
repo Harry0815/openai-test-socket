9263d2421bfea4f61ec50a6410b6ab034c507488
nxf-haraldschmidt <harald.schmidt@nexum.com>
2025-12-28 17:53:33 +0100

    feature-libs created

--- apps/test-socket/src/app/app.module.ts
+++ apps/test-socket/src/app/app.module.ts
@@ -1,35 +1,17 @@
 import { Module } from '@nestjs/common';
-import { PingController } from './controllers/ping.controller';
-import { HealthController } from './controllers/health.controller';
-import { EchoController } from './controllers/echo.controller';
-import { StaticController } from './controllers/static.controller';
-import { PingService } from './services/ping.service';
-import { HealthService } from './services/health.service';
-import { EchoService } from './services/echo.service';
-import { AudioService } from './services/audio.service';
-import { AudioRecordingService } from './services/recording.service';
-import { SocketLiveAudioService } from './services/live-audio.service';
 import {
   FeatureTranslateModule,
 } from '@test-socket/feature-translate';
+import { FeatureFirstTestsModule } from '@test-socket/feature-first-tests';
 
 @Module({
   imports: [
-    FeatureTranslateModule
+    FeatureTranslateModule,
+    FeatureFirstTestsModule
   ],
   controllers: [
-    PingController,
-    HealthController,
-    EchoController,
-    StaticController
   ],
   providers: [
-    PingService,
-    HealthService,
-    EchoService,
-    AudioService,
-    AudioRecordingService,
-    SocketLiveAudioService,
   ],
 })
 export class AppModule {}
